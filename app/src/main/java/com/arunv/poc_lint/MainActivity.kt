5ef9078e2728bb42f26bd51eb0d6b0b71db4475a
Balamurugan Selvam <balamurugan.selvam@hcl.com>
2020-12-10 10:50:08 +0530

    Initial commit

---
+++ app/src/main/java/com/arunv/poc_lint/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arunv.poc_lint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
